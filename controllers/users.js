3c3741a977fb95d5e65befdc3793dcd829a777a8
Rozovika Ivan <ivanrozovika@yandex.ru>
2023-12-10 17:09:13 +0300

    small fixes

--- controllers/users.js
+++ controllers/users.js
@@ -9,9 +9,9 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUserById = (req, res) => {
-  const { userId } = req.params;
+  const { userID } = req.params;
   userSchema
-    .findById(userId)
+    .findById(userID)
     .then((user) => {
       if (!user) {
         return res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
