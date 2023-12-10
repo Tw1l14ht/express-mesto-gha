e87aafa587933b4ecf7da78de9b5ed9954641cd7
Rozovika Ivan <ivanrozovika@yandex.ru>
2023-12-10 15:47:25 +0300

    fix endp

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const cardsRouter = require('./cards');
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 
-router.use('/', (req, res) => {
+router.use('*', (req, res) => {
   res.status(404).send({ message: 'Not Found' });
 });
 
