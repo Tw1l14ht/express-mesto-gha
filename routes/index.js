689313678dc731a252b1c2a1a27927096a4d2df2
Rozovika Ivan <ivanrozovika@yandex.ru>
2023-12-10 17:30:57 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 
-router.use('/users', usersRouter);
-router.use('/cards', cardsRouter);
-
 router.use('*', (req, res) => {
   res.status(404).send({ message: 'Not Found' });
 });
+
+router.use('/users', usersRouter);
+router.use('/cards', cardsRouter);
 
 module.exports = router;
