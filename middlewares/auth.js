0d5720bd3d37826a8b80920ac64af82014f3205a
Rozovika Ivan <ivanrozovika@yandex.ru>
2023-12-15 00:29:10 +0300

    last err

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,7 +12,7 @@ module.exports = (req, res, next) => {
     const token = authorization.replace('Bearer ', '');
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return next(err);
+    return next(new AuthError('Необходима авторизация'));
   }
   req.user = payload;
   next();
