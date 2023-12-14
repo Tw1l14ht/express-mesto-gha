47ecffa9a58b72ddbae00938e084de898c0a46a2
Rozovika Ivan <ivanrozovika@yandex.ru>
2023-12-14 21:36:12 +0300

    some fix

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,15 @@ const {
   getUsers, getUserById, updateUser, updateAvatar, getMyUser,
 } = require('../controllers/users');
 const { linkVerify } = require('../utils/regexFunc');
+const { idVerify } = require('../utils/constans');
 
 userRoutes.get('/', getUsers);
 userRoutes.get('/me', getMyUser);
-userRoutes.get('/:userId', getUserById);
+userRoutes.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().required().custom(idVerify),
+  }),
+}), getUserById);
 userRoutes.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
